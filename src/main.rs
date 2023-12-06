6346acd38445c9d875838ed0020a9b41c3fc4805
David Ellis <isv.damocles@gmail.com>
2023-12-06 17:20:56 -0600

    Re-organize things so each sub-command is its own function, and Rust's memory model is starting to come back to me -- dropping a clone() by passing ownership to the match block

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ enum Commands {
     Suggest,
 }
 
-fn init(url: Option<String>, dest: &OsString) {
+fn init(url: Option<String>, dest: OsString) {
     let url = url.clone().unwrap_or("https://huggingface.co/TheBloke/CodeLlama-13B-Instruct-GGUF/resolve/main/codellama-13b-instruct.Q4_K_M.gguf?download=true".to_string());
     match get(&url) {
         Ok(mut res) => {
@@ -52,6 +52,64 @@ fn init(url: Option<String>, dest: &OsString) {
     };
 }
 
+fn check() {
+    println!("TODO: Check with clarify (look for code lacking documentation and report it, also look for code that appears to have out-of-date documentation)");
+}
+
+fn suggest(model_path: OsString) {
+    println!("TODO: Do this more correctly (it's what the code below is starting to do, generate documentation comments for existing code)");
+    let num_threads: i32 = available_parallelism().unwrap().get() as i32;
+    let model_path = match std::env::var("GGUF") {
+        Ok(v) => v,
+        Err(_) => model_path
+            .into_string()
+            .expect("What platform has non-Unicode-mappable paths?"),
+    };
+
+    let model_options = ModelOptions {
+        n_gpu_layers: 43,
+        numa: true,
+        ..Default::default()
+    };
+
+    let llama = LLama::new(model_path, &model_options).unwrap();
+
+    // TODO: Don't hardwire this
+    let mut file = File::open("./src/main.rs").expect("Unable to open file");
+    let mut src = String::new();
+    file.read_to_string(&mut src).expect("Unable to read file");
+    let syntax = syn::parse_file(&src).unwrap();
+
+    for item in syntax.items {
+        match item {
+            syn::Item::Fn(i) => {
+                let predict_options = PredictOptions {
+                    tokens: 0,
+                    threads: num_threads,
+                    token_callback: Some(Box::new(|token| {
+                        print!("{}", token);
+
+                        true
+                    })),
+                    ..Default::default()
+                };
+                let out = llama
+                    .predict(
+                        format!(
+                            include_str!("./prompts/rs.txt"),
+                            i.span().source_text().unwrap().replace("```", "\\`\\`\\`")
+                        ),
+                        predict_options,
+                    )
+                    .unwrap();
+                println!("output: {}", out);
+                //println!("fn: {}", i.span().source_text().unwrap());
+            }
+            _ => (),
+        }
+    }
+}
+
 fn main() {
     let model_path = match data_dir() {
         Some(mut data_path) => {
@@ -61,63 +119,9 @@ fn main() {
         None => "~/.clarify_model.gguf".into(), // For the BSDs, I guess?
     };
     let cli = Cli::parse();
-    match &cli.command {
-        Commands::Init { url } => init(url.clone(), &model_path),
-        Commands::Check => {
-            println!("TODO: Check with clarify (look for code lacking documentation and report it, also look for code that appears to have out-of-date documentation)");
-        }
-        Commands::Suggest => {
-            println!("TODO: Do this more correctly (it's what the code below is starting to do, generate documentation comments for existing code)");
-            let num_threads: i32 = available_parallelism().unwrap().get() as i32;
-            let model_path = match std::env::var("GGUF") {
-                Ok(v) => v,
-                Err(_) => model_path
-                    .into_string()
-                    .expect("What platform has non-Unicode-mappable paths?"),
-            };
-
-            let model_options = ModelOptions {
-                n_gpu_layers: 43,
-                numa: true,
-                ..Default::default()
-            };
-
-            let llama = LLama::new(model_path, &model_options).unwrap();
-
-            // TODO: Don't hardwire this
-            let mut file = File::open("./src/main.rs").expect("Unable to open file");
-            let mut src = String::new();
-            file.read_to_string(&mut src).expect("Unable to read file");
-            let syntax = syn::parse_file(&src).unwrap();
-
-            for item in syntax.items {
-                match item {
-                    syn::Item::Fn(i) => {
-                        let predict_options = PredictOptions {
-                            tokens: 0,
-                            threads: num_threads,
-                            token_callback: Some(Box::new(|token| {
-                                print!("{}", token);
-
-                                true
-                            })),
-                            ..Default::default()
-                        };
-                        let out = llama
-                            .predict(
-                                format!(
-                                    include_str!("./prompts/rs.txt"),
-                                    i.span().source_text().unwrap().replace("```", "\\`\\`\\`")
-                                ),
-                                predict_options,
-                            )
-                            .unwrap();
-                        println!("output: {}", out);
-                        //println!("fn: {}", i.span().source_text().unwrap());
-                    }
-                    _ => (),
-                }
-            }
-        }
+    match cli.command {
+        Commands::Init { url } => init(url, model_path),
+        Commands::Check => check(),
+        Commands::Suggest => suggest(model_path),
     }
 }
