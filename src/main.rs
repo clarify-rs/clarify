6157a80685272d514d85a9bee6ac6797e7333002
David Ellis <isv.damocles@gmail.com>
2023-12-06 10:52:20 -0600

    Add clap and start laying out the kinds of commands clarify will have

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,40 @@ use std::fs::File;
 use std::io::Read;
 use std::thread::available_parallelism;
 
+use clap::{Parser, Subcommand};
 use llama_cpp_rs::{
     options::{ModelOptions, PredictOptions},
     LLama,
 };
 use syn::spanned::Spanned;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+  #[command(subcommand)]
+  command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+  Init,
+  Check,
+  Suggest,
+}
+
 fn main() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Init => {
+          println!("TODO: Initialize clarify (download a Llama model, perhaps asking which one they want)");
+        },
+        Commands::Check => {
+          println!("TODO: Check with clarify (look for code lacking documentation and report it, also look for code that appears to have out-of-date documentation)");
+        },
+        Commands::Suggest => {
+          println!("TODO: Do this more correctly (it's what the code below is starting to do, generate documentation comments for existing code)");
+        }
+    }
     let num_threads: i32 = available_parallelism().unwrap().get() as i32;
     let model_path = match std::env::var("GGUF") {
         Ok(v) => v.into(),
