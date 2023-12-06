66bf20e98ff0a3fe901e1ee27c318413c98c6067
David Ellis <isv.damocles@gmail.com>
2023-12-05 19:43:51 -0600

    Move the prompt into a prompts directory

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use std::fs::File;
 use std::io::Read;
 use std::thread::available_parallelism;
 
-use syn::spanned::Spanned;
 use llama_cpp_rs::{
     options::{ModelOptions, PredictOptions},
     LLama,
 };
+use syn::spanned::Spanned;
 
 fn main() {
     let num_threads: i32 = available_parallelism().unwrap().get() as i32;
@@ -30,41 +30,31 @@ fn main() {
     let syntax = syn::parse_file(&src).unwrap();
 
     for item in syntax.items {
-      match item {
-        syn::Item::Fn(i) => {
-          let predict_options = PredictOptions {
-              tokens: 0,
-              threads: num_threads,
-              token_callback: Some(Box::new(|token| {
-                  print!("{}", token);
+        match item {
+            syn::Item::Fn(i) => {
+                let predict_options = PredictOptions {
+                    tokens: 0,
+                    threads: num_threads,
+                    token_callback: Some(Box::new(|token| {
+                        print!("{}", token);
 
-                  true
-              })),
-              ..Default::default()
-          };
-          let out = llama
-              .predict(
-                  format!(r#"
-Consider the following rust function:
-
-```rs
-{}
-```
-
-There is no rustdoc comment above the function describing its purpose.
-
-This is not good for future developers who encounter this function in the future. So, after reading the code to understand what it is doing, we add the rustdoc comment, and the full function with annotations is as follows:
-
-```rs
-
-      "#, i.span().source_text().unwrap().replace("```", "\\`\\`\\`")),
-                  predict_options,
-              )
-              .unwrap();
-          println!("output: {}", out);
-          //println!("fn: {}", i.span().source_text().unwrap());
-        },
-        _ => (),
-      }
+                        true
+                    })),
+                    ..Default::default()
+                };
+                let out = llama
+                    .predict(
+                        format!(
+                            include_str!("./prompts/rs.txt"),
+                            i.span().source_text().unwrap().replace("```", "\\`\\`\\`")
+                        ),
+                        predict_options,
+                    )
+                    .unwrap();
+                println!("output: {}", out);
+                //println!("fn: {}", i.span().source_text().unwrap());
+            }
+            _ => (),
+        }
     }
 }
